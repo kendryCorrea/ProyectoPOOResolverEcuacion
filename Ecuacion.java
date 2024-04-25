0e23332cf6d1d3ad8ce4f876e38bc62e4525d54a
amandasanchezhdz <159464289+amandasanchezhdz@users.noreply.github.com>
2024-04-25 13:03:49 -0400

    Update Ecuacion.java
    Agregando resolver()

--- Ecuacion.java
+++ Ecuacion.java
@@ -1,5 +1,8 @@
-public class Ecuacion {                                                                                                                                    public class Ecuacion {
+public class Ecuacion {                                                                                                                                   
     private double a;
     private double b;
     private double c;  
 }
+public double[] resolver() {
+    double discriminante = Math.pow(b, 2) - 4 * a * c;
+    double x1, x2;
