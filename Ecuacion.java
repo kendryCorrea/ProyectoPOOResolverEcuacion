fe07986db9608543ea037b068ff1c9dc61180700
AlejandroPombrol <160505510+AlejandroPombrol@users.noreply.github.com>
2024-04-26 10:26:50 -0400

    Update Ecuacion.java

--- Ecuacion.java
+++ Ecuacion.java
@@ -6,3 +6,17 @@ public class Ecuacion {
 public double[] resolver() {
     double discriminante = Math.pow(b, 2) - 4 * a * c;
     double x1, x2;
+    if (discriminante > 0) {
+            x1 = (-b + Math.sqrt(discriminante)) / (2 * a);
+            x2 = (-b - Math.sqrt(discriminante)) / (2 * a);
+    } else if (discriminante == 0) {
+            x1 = -b / (2 * a);
+            x2 = x1;
+     } else {
+            return new double[0];
+     }
+
+        return new double[]{x1, x2};
+
+    }
+}
